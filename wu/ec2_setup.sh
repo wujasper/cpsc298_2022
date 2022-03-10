6c5f78e2854a26a96bc9601c3aa3f633ad3de448
Jasper Wu <jaswu@chapman.edu>
2022-03-10 12:17:34 -0800

    change wcode to jcode

--- wu/ec2_setup.sh
+++ wu/ec2_setup.sh
@@ -2,8 +2,8 @@
 yum update -y
 yum install httpd -y
 yum install git -y 
-mkdir /wcode
-cd /wcode
+mkdir /jcode
+cd /jcode
 git clone --branch wu https://github.com/wujasper/cpsc298_2022.git
 sed -e "s/REPLACE_DATE/$(date)/" cpsc298_2022/wu/html/index.html | tee /var/www/html/index.html 
 service httpd start
